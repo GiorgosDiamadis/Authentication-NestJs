a154e1dba48372be17b9f622881f8f50275a84e0
GiorgosDiamadis <diamantisgiorgos2@gmail.com>
2022-04-12 00:33:55 +0300

    commit

--- br/src/app.module.ts
+++ br/src/app.module.ts
@@ -1,14 +1,12 @@
 import {Module} from '@nestjs/common';
-import {BooksModule} from "./books/books.module";
 import {AppController} from './app.controller';
 import {AppService} from './app.service';
 import {MongooseModule} from "@nestjs/mongoose";
 import {UsersModule} from "./users/users.module";
-import {AuthService} from "./auth/service/auth.service";
 import {AuthModule} from "./auth/auth.module";
 
 @Module({
-    imports: [BooksModule, AuthModule, UsersModule, MongooseModule.forRoot("mongodb://root:root@database:27017/book-rental?authSource=admin")],
+    imports: [AuthModule, UsersModule, MongooseModule.forRoot("mongodb://root:root@database:27017/book-rental?authSource=admin")],
     controllers: [AppController],
     providers: [AppService],
 
